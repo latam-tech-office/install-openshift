fb3b769a4afdaa410eccb6ee2cb7f780ce17cebd
Mauricio "Maltron" Leal <maltron@gmail.com>
2018-08-15 14:44:36 -0300

    Removed Step3 script so it won't conflict with different user

--- step3_install_openshift.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-set -xe
-pubkey=/home/mauricio/.ssh/id_rsa.pub
-ssh -i ${pubkey%.*} -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no root@35.196.23.164 "sudo /root/install_openshift.sh"
