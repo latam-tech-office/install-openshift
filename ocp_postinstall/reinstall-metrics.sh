35d5d339b10928a1e66eb516854e3068de6676bb
Mauricio Leal <maltron@gmail.com>
2018-08-24 10:08:39 -0300

    Making reinstall-metrics.sh executable

--- ocp_postinstall/reinstall-metrics.sh
+++ ocp_postinstall/reinstall-metrics.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 set -xe
 ansible-playbook /usr/share/ansible/openshift-ansible/playbooks/openshift-metrics/config.yml -e openshift_metrics_install_metrics=False
 ansible-playbook /usr/share/ansible/openshift-ansible/playbooks/openshift-metrics/config.yml
